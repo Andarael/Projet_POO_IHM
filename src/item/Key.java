8d8f4baae882e3ea3c1898efd257acb7598ceafd
josur <josuraad@gmail.com>
2020-12-06 11:22:52 +0100

    Key now have colors this is what determines if a key equals another

--- src/item/Key.java
+++ src/item/Key.java
@@ -1,8 +1,43 @@
 package item;
 
+import utils.Col;
+import static utils.Col.*;
+
 public class Key extends Item {
 
-    public Key(String name, String description) {
+    private final Col color;
+
+    public Key(String name, String description, Col color) {
         super(name, description);
+
+        if (color == null)
+            color = RESET;
+
+        this.color = color;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Key key = (Key) o;
+
+        return color == key.color;
+    }
+
+    @Override
+    public String getSimpleDisplay() {
+        return colorize(super.getSimpleDisplay(), color);
+    }
+
+    @Override
+    public String getDisplay() {
+        return colorize(super.getDisplay(), color);
+    }
+
+    @Override
+    public int hashCode() {
+        return color.hashCode();
     }
 }
---
+++ tests/KeyTest.java
@@ -0,0 +1,48 @@
+import item.Item;
+import item.Key;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import utils.Col;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static utils.Col.*;
+
+class KeyTest {
+
+    private Item k1;
+    private Item k2;
+    private Item k3;
+    private Item k4;
+
+    @BeforeEach
+    void setUp() {
+        k1 = new Key("blue_key", "a finely crafted blue key", BLUE);
+        k2 = new Key("red_key", "the  R E D  key", RED);
+        k3 = new Key("yellow_key", "a vibrant Yellow Key", YELLOW);
+        k3 = new Key(null, null, null);
+    }
+
+    @Test
+    void testEquals() {
+        Item k = new Key("blue_key", "a finely crafted blue key", BLUE);
+
+        assertEquals(k,k1);
+
+        k = new Key("not_blue_key", "a finely crafted blue key", BLUE);
+        assertEquals(k,k1);
+
+        assertNotEquals(k1,k2);
+
+        System.out.println(k1.hashCode());
+    }
+
+    @Test
+    void Display() {
+        System.out.println(k1.getSimpleDisplay());
+        System.out.println(k1.getDisplay());
+        System.out.println(k2.getDisplay());
+        System.out.println(k3.getDisplay());
+
+        assertTrue(k1.getDisplay().contains(BLUE.toString()));
+    }
+}
