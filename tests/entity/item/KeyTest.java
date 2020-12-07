3026f5eef1ad6acaca4b0c2e8a09bc50e4317c84
josur <josuraad@gmail.com>
2020-12-07 06:59:54 +0100

    updated equals()

--- tests/entity/item/KeyTest.java
+++ tests/entity/item/KeyTest.java
@@ -2,7 +2,6 @@ package entity.item;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import utils.Col;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static utils.Col.*;
@@ -12,33 +11,32 @@ class KeyTest {
     private Item k1;
     private Item k2;
     private Item k3;
-    private Item k4;
 
     @BeforeEach
     void setUp() {
         k1 = new Key("blue_key", "a finely crafted blue key", BLUE);
         k2 = new Key("red_key", "the  R E D  key", RED);
-        k3 = new Key("yellow_key", "a vibrant Yellow Key", YELLOW);
         k3 = new Key(null, null, null);
     }
 
     @Test
     void testEquals() {
-        Item k = new Key("blue_key", "a finely crafted blue key", BLUE);
+        assertEquals(k1,k1);
 
+        Item k = new Key("not a blue key !", "definitely not blue", BLUE);
         assertEquals(k,k1);
-
-        k = new Key("not_blue_key", "a finely crafted blue key", BLUE);
-        assertEquals(k,k1);
+        // en réalité la clé k est bleu
 
         assertNotEquals(k1,k2);
 
-        System.out.println(k1.hashCode());
+        Object o = new Object();
+        assertNotEquals(k1,o);
+
+        assertNotEquals(null, k1);
     }
 
     @Test
     void Display() {
-        System.out.println(k1.getSimpleDisplay());
         System.out.println(k1.getDisplay());
         System.out.println(k2.getDisplay());
         System.out.println(k3.getDisplay());
