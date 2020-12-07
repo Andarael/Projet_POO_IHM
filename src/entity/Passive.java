3c716c5c4f16eae1b398216684dc47ec5ab7834b
josur <josuraad@gmail.com>
2020-12-07 12:12:53 +0100

    Implemented Passive Entity

---
+++ src/entity/Passive.java
@@ -0,0 +1,21 @@
+package entity;
+
+import interfaces.Talkable;
+
+public class Passive extends Being implements Talkable {
+
+    public static final String DEFAULT_DIALOGUE = "Hello adventurer, i am old";
+
+    private final String dialogue;
+
+    public Passive(String name, String description, String dialogue) {
+        super(name, description);
+        if (dialogue == null)
+            dialogue = DEFAULT_DIALOGUE;
+        this.dialogue = dialogue;
+    }
+
+    public String getDialogue() {
+        return dialogue;
+    }
+}
