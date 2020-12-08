34c5bfde486c197a7d5b610a7a854c67e2fe49cd
josur <josuraad@gmail.com>
2020-12-08 09:39:18 +0100

    Made BowTest and corrected Bow

--- src/entity/item/Bow.java
+++ src/entity/item/Bow.java
@@ -8,23 +8,47 @@ public class Bow extends Weapon {
 
     private int arrows;
 
+    public static final double DEFAULT_WEIGHT = 3.0;
+
     private static final int ARROW_POWER = new Arrow().getPower();
 
     public Bow(String name, String description, double weight, int value, int damage) {
         super(name, description, weight, value, damage);
+        PREFIX = "BOW  ";
+        arrows = 0;
     }
 
+    public Bow(String name, String description, int damage) {
+        this(name, description, DEFAULT_WEIGHT, damage, damage);
+    }
 
     public void addArrow() {
         this.arrows ++;
     }
 
-    @Override
-    public int getPower() {
+    public int getArrows() {
+        return arrows;
+    }
+
+    public int getPowerNoConsume() {
         if (arrows < 1)
             return super.getPower();
         else
-            arrows --;
             return super.getPower() + ARROW_POWER;
+    }
+
+    @Override
+    public int getPower() {
+        int result;
+        result = getPowerNoConsume();
+        if (arrows > 0)
+            arrows--;
+        return result;
+    }
+
+    @Override
+    public String getDisplay() {
+        return super.getDisplay()
+                + "arrows : " + arrows;
     }
 }
---
+++ tests/entity/item/BowTest.java
@@ -0,0 +1,68 @@
+package entity.item;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BowTest {
+
+    private Arrow a;
+    private Bow b;
+
+    @BeforeEach
+    void setUp() {
+        a = new Arrow();
+        b = new Bow("IronBow", "a bow made of iron", 6.0, 5, 4);
+    }
+
+    @Test
+    void Bow() {
+        b = new Bow("WoodenBow", null, 1);
+        assertEquals(Bow.DEFAULT_WEIGHT, b.getWeight());
+        assertEquals(b.getDamage(), b.getValue());
+        assertEquals(0,b.getArrows());
+    }
+
+    @Test
+    void addArrow() {
+        b.addArrow();
+        assertEquals(1,b.getArrows());
+
+        b.addArrow();
+        b.addArrow();
+        b.addArrow();
+        assertEquals(4,b.getArrows());
+    }
+
+    @Test
+    void getArrows() {
+        addArrow();
+    }
+
+    @Test
+    void getPower() {
+        assertEquals(4,b.getPowerNoConsume());
+        b.addArrow();
+        assertEquals(4+a.getPower(),b.getPower());
+        assertEquals(4,b.getPowerNoConsume());
+
+
+    }
+
+    @Test
+    void getDisplay() {
+        for (int i = 0; i < 10; i++)
+            b.addArrow();
+
+        assertTrue(b.getDisplay().contains("10"));
+    }
+
+    @Test
+    void getPowerNoConsume() {
+        assertEquals(4,b.getPowerNoConsume());
+        b.addArrow();
+
+        assertEquals(4+a.getPower(), b.getPowerNoConsume());
+    }
+}
