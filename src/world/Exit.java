61ceed9f7e55c41ac36f33c11a3a19551b3e8184
N-hox <florian.portrait86@gmail.com>
2020-12-08 08:09:20 +0100

    ajout direction

--- src/world/Exit.java
+++ src/world/Exit.java
@@ -1,14 +1,21 @@
 package world;
 
+import java.awt.*;
+
 public class Exit {
     protected final Place destination;
+    private String direction;
 
-    public Exit(Place destination){
+    public Exit(Place destination, String direction){
         this.destination = destination;
+        this.direction = direction;
     }
 
     public Place getDestination() {
         return destination;
+    }
+    public String getDirection(){
+        return this.direction;
     }
 
     public Place goIn(){
