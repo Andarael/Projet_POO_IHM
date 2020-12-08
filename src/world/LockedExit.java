4ba0dcb19e49613403807e44959816ac111e558d
N-hox <florian.portrait86@gmail.com>
2020-12-08 08:11:34 +0100

    ajout color et modif de unlock()

--- src/world/LockedExit.java
+++ src/world/LockedExit.java
@@ -1,13 +1,20 @@
 package world;
 
+import entity.item.Key;
+import utils.Col;
+
+import java.awt.*;
+
 public class LockedExit extends Exit {
     private boolean isLocked;
     private final Place origine;
+    private final Col color;
 
-    public LockedExit(Place destination, Place origine) {
-        super(destination);
+    public LockedExit(Place destination, Place origine, String direction, Col color) {
+        super(destination, direction);
         this.origine = origine;
         this.isLocked = true;
+        this.color = color;
     }
 
     public boolean canGo(){
@@ -17,6 +24,7 @@ public class LockedExit extends Exit {
     @Override
     public Place goIn(){
         if (!this.canGo()){
+            System.out.println("Exit is locked");
             return this.origine;
         }else {
             return this.destination;
@@ -27,16 +35,27 @@ public class LockedExit extends Exit {
         return this.isLocked;
     }
 
-    public void unLock() {
-        this.isLocked = false;
+    public Place getOrigine() {
+        return this.origine;
+    }
+
+    public Col getColor(){
+        return this.color;
+    }
+
+    public void unLock(LockedExit exit, Key key) {
+        if (exit.getColor() == key.getColor()){
+            this.isLocked = false;
+        }else{
+            System.out.println("Please use the key required for this outing");
+        }
+
     }
 
     public void lock(){
         this.isLocked = true;
     }
 
-    public Place getOrigine() {
-        return origine;
-    }
+
 
 }
