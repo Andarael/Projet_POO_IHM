71a26c244a2fd52049b0b638361ccd9a0524f158
josur <josuraad@gmail.com>
2020-12-09 00:01:50 +0100

    Implemented Drawable.java

--- src/interfaces/Drawable.java
+++ src/interfaces/Drawable.java
@@ -3,7 +3,6 @@ package interfaces;
 public interface Drawable extends Describable {
 
     default String draw() {
-        System.out.println(getShortName().trim());
         return getShortName().trim();
     }
 }
