5f157e324902201e9289bd684733905b0c553bc5
josur <josuraad@gmail.com>
2020-12-09 07:06:34 +0100

    Created test for world

---
+++ tests/world/WorldTest.java
@@ -0,0 +1,87 @@
+package world;
+
+import entity.place.Place;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WorldTest {
+
+    World w;
+    private Place p1;
+    private Place p2;
+    private Place p3;
+    private Place p4;
+
+    @BeforeEach
+    void setUp() {
+        w = new World();
+
+        p1 = new Place("p1");
+        p2 = new Place("p2");
+        p3 = new Place("p3");
+        p4 = new Place("p4");
+    }
+
+    @Test
+    void getCurrentPlace() {
+    }
+
+    @Test
+    void setCurrentPlace() {
+    }
+
+    @Test
+    void testSetCurrentPlace() {
+    }
+
+    @Test
+    void addPlace() {
+        w.addPlace(p1);
+        w.addPlace(p2);
+        w.addPlace(p3);
+        w.addPlace(p4);
+
+        int size = w.size();
+        System.out.println(w);
+
+        w.addPlace(p1);
+        System.out.println(w);
+
+        assertEquals(size, w.size());
+
+    }
+
+    @Test
+    void isEnd() {
+    }
+
+    @Test
+    void getPlayer() {
+    }
+
+    @Test
+    void getPlace() {
+    }
+
+    @Test
+    void testGetPlace() {
+    }
+
+    @Test
+    void end() {
+    }
+
+    @Test
+    void size() {
+    }
+
+    @Test
+    void hasWin() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
