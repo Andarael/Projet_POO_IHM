280173abc3ec8a02543d146f665d7c0591ebde8d
josur <josuraad@gmail.com>
2020-12-10 02:12:47 +0100

    added null check

--- src/game/EnumCommand.java
+++ src/game/EnumCommand.java
@@ -3,28 +3,33 @@
 package game;
 
 public enum EnumCommand {
+
     DROP("Drops an item from your inventory in the room \n" +
          "For example : Drop pog -> you dropped a pog on the floor",
          "D",
          1,
          1
     ),
+
     GO("Allows you to go from a Room to another \n" +
        "For example : GO Labo -> Enters into the Laboratory if possible",
        "G",
        1,
        1
     ),
+
     HELP("HELP help",
          "H",
          0,
          0
     ),
+
     INVENTORY("Displays the content of your inventory",
               "I",
               0,
               0
     ),
+
     LOOK("Allows you to look at any entity in the Room, \n" +
          "Such as Monsters, NPCs, Containers, Exits, or the room itself \n" +
          "For example : Look -> looks a the room \n" +
@@ -33,17 +38,20 @@ public enum EnumCommand {
          0,
          1
     ),
+
     QUIT("Quits the game",
          "Q",
          0,
          0
     ),
+
     TAKE("Takes an item from a Container, \n" +
          "For example : Take Chest Chicken -> add the chicken from the chest to your inv if possible",
          "T",
          1,
          2
     ),
+
     USE("Use an Item (possibly on another Item) \n" +
         "For example : Use Chicken -> You eat the chicken it gives you back 5 hp \n" +
         "            : Use Arrow Bow -> You add 1 arrow to the Bow",
@@ -51,18 +59,21 @@ public enum EnumCommand {
         1,
         2
     ),
-    ATTACK("Attack a Npc in the current place\n" +
+
+    ATTACK("Attack a Being in the current place\n" +
            "For example : Attack Cyclops -> You attack the Cyclops and deals him 5 damage",
            "A",
            1,
            1
     ),
+
     BUY("Buy an item to the Npc in the current place\n" +
         "For example : Buy Merchant sword -> You bought a sword from the merchant and gave him 5 golds",
         "B",
         2,
         2
     ),
+
     SELL("Sell an item to the Npc in the current place\n" +
          "For example : Sell Merchant sword -> You sold a sword from the merchant and won 5 golds",
          "S",
@@ -83,6 +94,9 @@ public enum EnumCommand {
     }
 
     public static EnumCommand getCommandFromString(String str) {
+        if (str == null)
+            return null;
+
         String temp = str.toUpperCase();
 
         for (EnumCommand command : EnumCommand.values()) {
