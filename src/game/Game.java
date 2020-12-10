a16d5a1a3f61ab853cd07410b1f787b94fe24511
josur <josuraad@gmail.com>
2020-12-10 02:13:18 +0100

    Created game, where the main game loop is hosted

---
+++ src/game/Game.java
@@ -0,0 +1,75 @@
+// Fichier par Josué Raad
+
+package game;
+
+import command.Command;
+import world.World;
+
+import java.util.List;
+
+import static game.Interpreter.*;
+
+import static command.Command.execute;
+
+public class Game {
+
+    private final World world;
+
+    public Game(int difficulty) {
+        this.world = new World(difficulty);
+    }
+
+    public boolean play() {
+
+
+        List<String> userInput;
+        boolean victory = false;
+        boolean death = false;
+
+        while (! (victory ||death)) {
+
+            displayWelcome();
+
+            world.getCurrentPlace().draw();
+
+            displayWaitingInput();
+
+            userInput = getUserInput();
+
+            Command.execute(world, userInput);
+
+            victory = world.hasWin();
+            death = world.getPlayer().isDead();
+
+            if (victory) {
+                displayVictory();
+            }
+            if (death) {
+                displayDeath();
+            }
+
+
+
+        }
+
+        return (victory);
+
+    }
+
+    private static void displayWaitingInput() {
+        // todo
+    }
+
+    private static void displayDeath() {
+        // todo
+    }
+
+    private static void displayVictory() {
+        // todo
+    }
+
+    private static void displayWelcome() {
+        // todo
+    }
+
+}
