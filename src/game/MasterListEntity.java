1bedfb93052d49ede39c20f2bda3954985167ab3
sortique <thibaud.devemy@orange.fr>
2020-12-10 22:31:46 +0100

    weapons and food

--- src/game/MasterListEntity.java
+++ src/game/MasterListEntity.java
@@ -29,6 +29,7 @@ public interface MasterListEntity {
     Weapon dagger = new Weapon("dagger", "a tiny dagger", 1, 1, 1);
     Bow hunters_bow = new Bow("hunter's bow", "a small but tough bow, made for sharpened arrows", 3, 3, 3);
     Arrow irons_arrow = new Arrow();
+    Weapon spear = new Weapon("spear", "a long stick with a piece of metal at the end", 4,4,4);
 
     // Food   ==================================================================
     Food chicken = new Food("chicken", "a raw chicken", 1, 2, 2);
@@ -37,6 +38,8 @@ public interface MasterListEntity {
     Food coca_zero = new Food("coca zero", "it just look like oil, but with no sugar in it", 1,5,20);
     Food big_cake = new Food("big cake", "a big lemon cake", 1,3,3);
     Food poison = new Food("weird-looking bottle", "i wouldn't drink this if i were you", 1,10,-15);
+    Food apple = new Food("apple","a mouthwatering apple",1,1,1);
+
 
     // Keys   ==================================================================
 
@@ -46,8 +49,20 @@ public interface MasterListEntity {
     // StaticContainers ========================================================
 
     // Hostiles ================================================================
+    Hostile orc = new Hostile("orc", "a green creature", 5, 5);
+    Hostile goblin = new Hostile("goblin", "a small and pathetic creature", 3, 3);
+    Hostile dragon = new Hostile("dragon", "a gigantic dragon with emerald reflections", 20, 20);
+    Hostile necromancer = new Hostile("necromancer", "a dark wizard in tattered clothes", 10, 10);
+    Hostile minotaur = new Hostile("minotaur", "a scary creature with a man's body but a bull's head", 8,8);
+    Hostile ghost = new Hostile("ghost", "you can barely see the outlines of this creature", 6,6);
+    Hostile skeleton = new Hostile("skeleton", "a human skeleton with shreds of flesh still attached to its bones", 4, 4);
+
 
     // Passive =================================================================
+    Passive ordinary_guy = new Passive("ordinary guy", "a human just like you", "Hello sir, the weather is nice, don't you think?");
+    Passive merchant = new Passive("merchant", "a big guy with all kinds of things in his bag", "Hello adventurer, would you like to take a look at my goods?");
+    Passive scandalf = new Passive("Scandalf the wizard", "He has a long beard and a pointy hat, you should listen to what he tells you", "Hello adventurer, and welcome to the dungeon of Xar Tsaroth ! Kill the dragon and you'll be a hero for ever");
+
 
     Set<Entity> entityMasterList = getAllEntities();
 
