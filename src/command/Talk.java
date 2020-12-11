610cb68f1693d082e86e379797d115322df2cf6d
josur <josuraad@gmail.com>
2020-12-11 06:40:27 +0100

    Implemented talk

--- src/command/Talk.java
+++ src/command/Talk.java
@@ -1,9 +1,32 @@
 package command;
 
+import entity.Container;
 import entity.place.Place;
+import interfaces.Talkable;
+
+import static utils.Printer.*;
 
 public interface Talk {
     static void talk(Place currentPlace, String arg1) {
-        //todo
+
+        Container npc = currentPlace.getContainerByName(arg1);
+
+        if (npc == null) {
+            printErr(arg1 + " is not here");
+            return;
+        }
+
+        if (!(npc instanceof Talkable)) {
+            printErr("You can't talk to " + arg1);
+            return;
+        }
+
+        printMsg(npc.getName() + "says :");
+        printDialogue(((Talkable) npc).talk());
+
+        if (!npc.isEmpty()) {
+            printDialogue("Here is what I may have for you :");
+            printMsg(npc.getInventoryDisplay());
+        }
     }
 }
