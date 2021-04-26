b8abb8f0bee44aa0136b531943486c68a3bf67ce
josur <josuraad@gmail.com>
2021-04-27 00:18:58 +0200

    Added utils

---
+++ src/controller/utils/Utils.java
@@ -0,0 +1,19 @@
+package controller.utils;
+
+public interface Utils {
+    static String pluralize(String string, int number, String plural) {
+        return number == 1 ? string : plural;
+    }
+
+    static String pluralize(String string, int number) {
+        return pluralize(string, number, string.trim() + 's');
+    }
+
+    static  String capitalize(String string) {
+        return string.substring(0,1).toUpperCase() + string.substring(1);
+    }
+
+    static String readable(String str) {
+        return str.replace('_', ' ').trim();
+    }
+}
